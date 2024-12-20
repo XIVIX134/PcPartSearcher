8a5bfa9741fd8a4d592c05b0bb7b0a9f9b66ad11
Loay mohamed <72279810+zoroxide@users.noreply.github.com>
2024-12-20 14:49:46 +0200

    Adding TODOs

---
+++ api/controllers/badr_controller.py
@@ -0,0 +1,5 @@
+from flask import jsonify
+
+#TODO: Implement the get_badr_items function
+def get_badr_items():
+    return jsonify({'badr': 'items'})
---
+++ api/controllers/sigma_controller.py
@@ -0,0 +1,6 @@
+from flask import jsonify
+
+
+# TODO: Implement the get_sigma_items function
+def get_sigma_items():
+    return jsonify({'sigma': 'items'})

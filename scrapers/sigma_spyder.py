10d7df2bca1ee2dfd58b8fd7d7eebcae07786611
Loay Mohamed <72279810+zoroxide@users.noreply.github.com>
2025-01-06 22:14:55 +0200

    minor comment

--- scrapers/sigma_spyder.py
+++ scrapers/sigma_spyder.py
@@ -9,6 +9,7 @@ url2 = "https://www.sigma-computer.com/subcategory?id=1&cname=Desktop&id2=2&scna
 
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+#change the url here as you like
 response = requests.get(url2, headers=headers)
 response.raise_for_status()
 

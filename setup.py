89c2bd4335af92c813c8dbe9c5cdb225308d3837
XIVIX <asserahmed134@gmail.com>
2025-01-26 10:47:28 +0200

    Update backend deployment script for package installation and enhance setup.py dependencies

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="pc-part-searcher",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=['app*', 'scrapers*']),
     install_requires=[
         'fastapi',
         'uvicorn',
         'requests',
         'beautifulsoup4',
-        'aiohttp'
+        'aiohttp',
+        'httpx',
+        'asyncio'
     ]
 )
